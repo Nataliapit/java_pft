675a2aaed26eeb7108274c68d08d78eda3c77bf7
Nataliapit <Nataliapit@mail.ru>
2022-05-18 13:33:18 +0300

    Функции и методы

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -1,14 +1,22 @@
 package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
-  public static <string> void main(String[] args) {
+  public static void main(String[] args) {
     String somebody = "world";
     System.out.println("Hello, " + somebody + "!");
+    hello("world");
+    hello("user");
+    hello("Natalia");
 
-    double l = 8.0;
-    double s = l * l;
-    System.out.println("Площадь квадрата со стороной " + l +" равна " + s);
+    Square s = new Square(5);
+    System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
+    Rectangle r = new Rectangle(4, 6);
+    System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b + " равна " + r.area());
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello, " + somebody + "!");
   }
 
 }
