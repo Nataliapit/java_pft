06ed5dae964b50439632db5e415f30a6475a2310
Nataliapit <Nataliapit@mail.ru>
2022-05-20 08:55:10 +0300

    Класс с функцией

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -12,11 +12,20 @@ public class MyFirstProgram {
     System.out.println("Площадь квадрата со стороной " + s.l + " равна " + s.area());
 
     Rectangle r = new Rectangle(4, 6);
-    System.out.println("Площадь прямоугольника со сторонами " + r.a +" и " + r.b + " равна " + r.area());
+    System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " равна " + r.area());
+
+    Point p1 = new Point(5, 8);
+    Point p2 = new Point(2, 12);
+    System.out.println("Расстояние между точками " + p1.x + "," + p1.y + " и " + p2.x + "," + p2.y + " равно " + distance(p1, p2));
   }
 
   public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double distance(Point p1, Point p2) {
+    return Math.sqrt(Math.pow(p1.x - p2.x,2) + Math.pow(p1.y - p2.y,2));
+
   }
 
 }
