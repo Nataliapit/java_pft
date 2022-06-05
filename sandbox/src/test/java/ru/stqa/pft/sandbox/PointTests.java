060164821a8e2f68378750824f7eb7d788e48e81
Nataliapit <Nataliapit@mail.ru>
2022-06-05 09:11:31 +0300

    Много тестов для Point

--- sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -9,13 +9,40 @@ public class PointTests {
     Point p1 = new Point(5, 8);
     Point p2 = new Point(2, 12);
 
-    Assert.assertEquals(p1.distance(p2),5.0);
-    Assert.assertEquals(p1.distance(p2),115.0);
-    Assert.assertEquals(p1.distance(p2),5);
+    Assert.assertEquals(p1.distance(p2), 5.0);
+  }
 
-    p1 = new Point(7, 10);
-    p2 = new Point(12, 4);
+  @Test
+  public void testDistance2() {
+    Point p1 = new Point(5, 8);
+    Point p2 = new Point(2, 12);
 
-    Assert.assertEquals(p1.distance(p2),5.0);
+    Assert.assertEquals(p1.distance(p2), 115.0);
+
+  }
+
+  @Test
+  public void testDistance3() {
+    Point p1 = new Point(5, 8);
+    Point p2 = new Point(2, 12);
+
+    Assert.assertEquals(p1.distance(p2), 5);
+
+  }
+
+  @Test
+  public void testDistance4() {
+    Point p1 = new Point(7, 10);
+    Point p2 = new Point(12, 4);
+
+    Assert.assertEquals(p1.distance(p2), 5.0);
+  }
+
+  @Test
+  public void testDistance5() {
+    Point p1 = new Point(7, 10);
+    Point p2 = new Point(12, 4);
+
+    Assert.assertEquals(p1.distance(p2), 7.81);
   }
 }
